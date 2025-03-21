80a0b9b6eb48be3c453276a9ff21a3a7adbce26a
sirconscious <mehdielbakouri0@gmail.com>
2025-03-21 23:19:48 +0000

    Modifing the login layout to look better

--- resources/views/Layouts/LoginLayout.blade.php
+++ resources/views/Layouts/LoginLayout.blade.php
@@ -6,31 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     @vite('resources/css/app.css')
-
-    <title>Document</title>
+    <title>SchoolManager</title>
 </head>
-<body >
-    
-{{-- The navBar --}}
-
-{{-- <nav class="bg-white border-gray-200 dark:bg-gray-900">
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SchoolManger</span>
-        </a>
-        <div class="flex items-center space-x-6 rtl:space-x-reverse">
-            <a href="#" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+<body class="bg-gradient-to-r from-blue-50 to-purple-50 dark:from-gray-900 dark:to-gray-800">
+    <!-- Main Container -->
+    <div class="min-h-screen w-full flex flex-col justify-center items-center p-4">
+        <!-- Logo and Title -->
+        <div class="text-center mb-8 transform transition-transform duration-500 hover:scale-105">
+            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-20 w-20" alt="Flowbite Logo" />
+                <span class="self-center text-5xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent dark:from-blue-400 dark:to-purple-400">
+                    SchoolManager
+                </span>
+            </a>
         </div>
-    </div>
-</nav> --}}
-<div class=" w-full flex gap-4 h-screen justify-center flex-col items-center">
-    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse ">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-15" alt="Flowbite Logo" />
-        <span class="self-center text-5xl font-semibold whitespace-nowrap dark:text-white">SchoolManger</span>
-    </a>
-    @yield('content') 
-</div>
 
+        <!-- Content Section -->
+        <div class="w-full max-w-4xl bg-white dark:bg-gray-800 rounded-lg shadow-xl p-8">
+            @yield('content')
+        </div>
+
+        <footer class="mt-8 text-center text-gray-600 dark:text-gray-400">
+            <p>&copy; 2023 SchoolManager. All rights reserved.</p>
+        </footer>
+    </div>
 </body>
 </html>
